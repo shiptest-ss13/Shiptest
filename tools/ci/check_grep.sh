d1af4b510f8615feac4b83e86b03386625d86067
AnturK <AnturK@users.noreply.github.com>
2021-01-16 20:18:32 +0100

    Adds custom icon mapping helper. (#56183)
    Allows embedding icon edits in your maps by fetching them from external host.
    This is intended for use with live-loaded away/event maps not standard ones.
    
    Also updates rustg defines to expose the additional options arguments. (https://github.com/tgstation/rust-g/pull/59)

--- tools/ci/check_grep.sh
+++ tools/ci/check_grep.sh
@@ -66,6 +66,10 @@ if ls _maps/*.json | grep -P "[A-Z]"; then
     echo "Uppercase in a map json detected, these must be all lowercase."
 	st=1
 fi;
+if grep -i '/obj/effect/mapping_helpers/custom_icon' _maps/**/*.dmm; then
+    echo "Custom icon helper found. Please include dmis as standard assets instead for built-in maps."
+    st=1
+fi;
 for json in _maps/*.json
 do
     filename="_maps/$(jq -r '.map_path' $json)/$(jq -r '.map_file' $json)"
