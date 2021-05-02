HolyTigerGod/Voidcrew

Fix Map JSON loading errors (#54563)
Also fixes that one runtime everyone runs into when they first load in

code/datums/map_config.dm
@@ -139,9 +139,11 @@
 
 	allow_custom_shuttles = json["allow_custom_shuttles"] != FALSE
 
-	if (!islist(json["jobs_changes"]))
-		log_world("map_config \"job_changes\" field is missing or invalid!")
-		return
+	if ("jobs_changes" in json)
+		if(!islist(json["jobs_changes"]))
+			log_world("map_config \"job_changes\" field is missing or invalid!")
+			return
+		job_changes = json["job_changes"]
 
 	defaulted = FALSE
 	return TRUE

shiptest-ss13/Shiptest

code/datums/map_config.dm
@@ -143,9 +143,11 @@ GLOBAL_VAR_INIT(next_mining_map, "random")
 
 	allow_custom_shuttles = json["allow_custom_shuttles"] != FALSE
 
-	if (!islist(json["jobs_changes"]))
-		log_world("map_config \"job_changes\" field is missing or invalid!")
-		return
+	if ("job_changes" in json)
+		if(!islist(json["job_changes"]))
+			log_world("map_config \"job_changes\" field is missing or invalid!")
+			return
+		job_changes = json["job_changes"]
 
 	defaulted = FALSE
 	return TRUE