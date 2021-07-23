cd5b32c5894df49a2924c2847dd7089532a507e3
Mark Suckerberg <mark@suckerberg.ga>
2021-07-23 01:50:00 -0500

    whoops

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -33,12 +33,5 @@ cp -r strings/* $1/strings/
 #find $1/_maps -name "*.dm" -type f -delete
 
 #dlls on windows
-<<<<<<< HEAD
-if [ "$(uname -o)" = "Msys" ]; then
-	cp ./*.dll $1/
-fi
-cp *byond-extools.* $1/ || true
-=======
 cp rust_g* $1/ || true
 cp auxtools/*auxmos.* $1/auxtools/ || true
->>>>>>> 6b5d452b2ff (The Great Auxtools Switch (#3767))
