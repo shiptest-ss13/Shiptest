3e74246da56b73aa3823370030ddea8e41408e7e
Matthew <matthew@tfaluc.com>
2021-09-05 00:56:47 -0400

    linter

--- tgui/packages/tgui/interfaces/HelmConsole.js
+++ tgui/packages/tgui/interfaces/HelmConsole.js
@@ -280,20 +280,22 @@ const ShipControlContent = (props, context) => {
   return (
     <Section
       title="Navigation"
-      buttons={(<>
-        <Button
-          tooltip="Undock"
-          tooltipPosition="left"
-          icon="sign-out-alt"
-          disabled={data.state !== 'idle'}
-          onClick={() => act('undock')} />
-        <Button
-          tooltip="Bluespace Jump"
-          tooltipPosition="left"
-          icon="sign-out-alt"
-          disabled={data.state !== 'flying'}
-          onClick={()=> act('bluespace_jump')} />
-      </>)}>
+      buttons={(
+        <>
+          <Button
+            tooltip="Undock"
+            tooltipPosition="left"
+            icon="sign-out-alt"
+            disabled={data.state !== 'idle'}
+            onClick={() => act('undock')} />
+          <Button
+            tooltip="Bluespace Jump"
+            tooltipPosition="left"
+            icon="sign-out-alt"
+            disabled={data.state !== 'flying'}
+            onClick={() => act('bluespace_jump')} />
+        </>
+      )}>
       {data.state === 'idle' && (
         <div className="NoticeBox">
           Ship Docked.
