8f0dfa40b03db0264e4fcea734ac7f07e8b2b9e1
Mark Suckerberg <mark@suckerberg.ga>
2021-09-04 19:16:01 -0500

    nvm

--- tools/ci/template_dm_generator.py
+++ tools/ci/template_dm_generator.py
@@ -3,12 +3,10 @@
 import os
 import sys
 
-folders = ["_maps\\RandomRuins", "_maps\\RandomZLevels", "_maps\\shuttles",
-           "_maps\\templates"]
+folders = ["_maps/RandomRuins", "_maps/RandomZLevels", "_maps/shuttles",
+           "_maps/templates"]
 
-excluded = ["_maps\\shuttles\\pirate", "_maps\\shuttles\\infiltrator"]
-
-generated = "_maps\\templates.dm"
+generated = "_maps/templates.dm"
 
 template_filenames = []
 
@@ -17,8 +15,6 @@ def find_dm(path):
     for dirpath, dirnames, filenames in os.walk(path):
         for name in filenames:
             if name.endswith(".dmm"):
-                if dirpath in excluded:
-                    continue
                 s = os.path.join(dirpath, name)
                 s = s.replace("_maps/","")
                 L.append(s)
