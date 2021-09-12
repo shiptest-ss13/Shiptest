HolyTigerGod/Voidcrew

Refactors tile reskinning (#54233)
* Refactors tile reskinning

* revert maxhealth changes

* what's with the extra spaces

* Include floors in the name changes

* tile fix and delete and create instead of change

* damn you github auto-resolve

* more atomization

code/game/turfs/open/floor/plasteel_floor.dm
@@ -4,91 +4,131 @@
 	broken_states = list("damaged1", "damaged2", "damaged3", "damaged4", "damaged5")
 	burnt_states = list("floorscorched1", "floorscorched2")
 
+
 /turf/open/floor/plasteel/examine(mob/user)
 	. = ..()
 	. += "<span class='notice'>There's a <b>small crack</b> on the edge of it.</span>"
+
 
 /turf/open/floor/plasteel/rust_heretic_act()
 	if(prob(70))
 		new /obj/effect/temp_visual/glowing_rune(src)
 	ChangeTurf(/turf/open/floor/plating/rust)
 
-/turf/open/floor/plasteel/update_icon()
-	. = ..()
-	if(!.)
+
+/turf/open/floor/plasteel/update_icon_state()
+	if(broken || burnt)
 		return
-	if(!broken && !burnt)
-		icon_state = icon_regular_floor
+	icon_state = base_icon_state
+
 
 /turf/open/floor/plasteel/airless
 	initial_gas_mix = AIRLESS_ATMOS
+
 /turf/open/floor/plasteel/telecomms
 	initial_gas_mix = TCOMMS_ATMOS
+
 /turf/open/floor/plasteel/icemoon
 	initial_gas_mix = ICEMOON_DEFAULT_ATMOS
 
 /turf/open/floor/plasteel/dark
 	icon_state = "darkfull"
+	base_icon_state = "darkfull"
+
 /turf/open/floor/plasteel/dark/airless
 	initial_gas_mix = AIRLESS_ATMOS
+
 /turf/open/floor/plasteel/dark/telecomms
 	initial_gas_mix = TCOMMS_ATMOS
+
 /turf/open/floor/plasteel/airless/dark
 	icon_state = "darkfull"
+	base_icon_state = "darkfull"
+
 /turf/open/floor/plasteel/dark/side
 	icon_state = "dark"
+	base_icon_state = "dark"
+
 /turf/open/floor/plasteel/dark/corner
 	icon_state = "darkcorner"
+	base_icon_state = "darkcorner"
+
 /turf/open/floor/plasteel/checker
 	icon_state = "checker"
+	base_icon_state = "checker"
 
 
 /turf/open/floor/plasteel/white
 	icon_state = "white"
+	base_icon_state = "white"
+
 /turf/open/floor/plasteel/white/side
 	icon_state = "whitehall"
+	base_icon_state = "whitehall"
+
 /turf/open/floor/plasteel/white/corner
 	icon_state = "whitecorner"
+	base_icon_state = "whitecorner"
+
 /turf/open/floor/plasteel/airless/white
 	icon_state = "white"
+	base_icon_state = "white"
+
 /turf/open/floor/plasteel/airless/white/side
 	icon_state = "whitehall"
+	base_icon_state = "whitehall"
+
 /turf/open/floor/plasteel/airless/white/corner
 	icon_state = "whitecorner"
+	base_icon_state = "whitecorner"
+
 /turf/open/floor/plasteel/white/telecomms
 	initial_gas_mix = TCOMMS_ATMOS
 
 /turf/open/floor/plasteel/airless/showroomfloor
 	icon_state = "showroomfloor"
+	base_icon_state = "showroomfloor"
 
 
 /turf/open/floor/plasteel/yellowsiding
 	icon_state = "yellowsiding"
+	base_icon_state = "yellowsiding"
+
 /turf/open/floor/plasteel/yellowsiding/corner
 	icon_state = "yellowcornersiding"
+	base_icon_state = "yellowcornersiding"
 
 
 /turf/open/floor/plasteel/recharge_floor
 	icon_state = "recharge_floor"
+	base_icon_state = "recharge_floor"
+
 /turf/open/floor/plasteel/recharge_floor/asteroid
 	icon_state = "recharge_floor_asteroid"
+	base_icon_state = "recharge_floor_asteroid"
 
 
 /turf/open/floor/plasteel/chapel
 	icon_state = "chapel"
+	base_icon_state = "chapel"
 
 /turf/open/floor/plasteel/showroomfloor
 	icon_state = "showroomfloor"
+	base_icon_state = "showroomfloor"
 
 
 /turf/open/floor/plasteel/solarpanel
 	icon_state = "solarpanel"
+	base_icon_state = "solarpanel"
+
 /turf/open/floor/plasteel/airless/solarpanel
 	icon_state = "solarpanel"
+	base_icon_state = "solarpanel"
 
 
 /turf/open/floor/plasteel/freezer
 	icon_state = "freezerfloor"
+	base_icon_state = "freezerfloor"
 
 /turf/open/floor/plasteel/freezer/airless
 	initial_gas_mix = AIRLESS_ATMOS
@@ -103,64 +143,89 @@
 
 /turf/open/floor/plasteel/kitchen_coldroom/freezerfloor
 	icon_state = "freezerfloor"
+	base_icon_state = "freezerfloor"
 
 
 /turf/open/floor/plasteel/grimy
 	icon_state = "grimy"
+	base_icon_state = "grimy"
 	tiled_dirt = FALSE
 
 /turf/open/floor/plasteel/cafeteria
 	icon_state = "cafeteria"
+	base_icon_state = "cafeteria"
 
 /turf/open/floor/plasteel/airless/cafeteria
 	icon_state = "cafeteria"
+	base_icon_state = "cafeteria"
 
 
 /turf/open/floor/plasteel/cult
-	icon_state = "cult"
 	name = "engraved floor"
+	icon_state = "cult"
+	base_icon_state = "cult"
 
 /turf/open/floor/plasteel/vaporwave
 	icon_state = "pinkblack"
+	base_icon_state = "pinkblack"
 
 /turf/open/floor/plasteel/goonplaque
-	icon_state = "plaque"
 	name = "commemorative plaque"
 	desc = "\"This is a plaque in honour of our comrades on the G4407 Stations. Hopefully TG4407 model can live up to your fame and fortune.\" Scratched in beneath that is a crude image of a meteor and a spaceman. The spaceman is laughing. The meteor is exploding."
+	icon_state = "plaque"
+	base_icon_state = "plaque"
 	tiled_dirt = FALSE
 
 /turf/open/floor/plasteel/cult/narsie_act()
 	return
+
 /turf/open/floor/plasteel/cult/airless
 	initial_gas_mix = AIRLESS_ATMOS
 
 
 /turf/open/floor/plasteel/stairs
 	icon_state = "stairs"
+	base_icon_state = "stairs"
 	tiled_dirt = FALSE
+
 /turf/open/floor/plasteel/stairs/left
 	icon_state = "stairs-l"
+	base_icon_state = "stairs-l"
+
 /turf/open/floor/plasteel/stairs/medium
 	icon_state = "stairs-m"
+	base_icon_state = "stairs-m"
+
 /turf/open/floor/plasteel/stairs/right
 	icon_state = "stairs-r"
+	base_icon_state = "stairs-r"
+
 /turf/open/floor/plasteel/stairs/old
 	icon_state = "stairs-old"
+	base_icon_state = "stairs-old"
 
 
 /turf/open/floor/plasteel/rockvault
 	icon_state = "rockvault"
+	base_icon_state = "rockvault"
+
 /turf/open/floor/plasteel/rockvault/alien
 	icon_state = "alienvault"
+	base_icon_state = "alienvault"
+
 /turf/open/floor/plasteel/rockvault/sandstone
 	icon_state = "sandstonevault"
+	base_icon_state = "sandstonevault"
 
 
 /turf/open/floor/plasteel/elevatorshaft
 	icon_state = "elevatorshaft"
+	base_icon_state = "elevatorshaft"
 
 /turf/open/floor/plasteel/bluespace
 	icon_state = "bluespace"
+	base_icon_state = "bluespace"
 
 /turf/open/floor/plasteel/sepia
 	icon_state = "sepia"
+	base_icon_state = "sepia"

shiptest-ss13/Shiptest

code/game/turfs/open/floor/plasteel_floor.dm
@@ -1,6 +1,7 @@
 /turf/open/floor/plasteel
 	icon = 'whitesands/icons/turf/floors/tiles.dmi'
 	color = COLOR_FLOORTILE_GRAY
+	base_icon_state = "tiled"
 	icon_state = "tiled"
 	floor_tile = /obj/item/stack/tile/plasteel
 	broken_states = list("broken0", "broken1", "broken2")
@@ -10,17 +11,17 @@
 	. = ..()
 	. += "<span class='notice'>There's a <b>small crack</b> on the edge where you can pry it with a <b>crowbar</b>.</span>"
 
-/turf/open/floor/plasteel/update_icon()
-	if(!..())
-		return 0
-	if(!broken && !burnt)
-		icon_state = icon_regular_floor
-
+/turf/open/floor/plasteel/update_icon_state()
+	if(broken || burnt)
+		return
+	icon_state = base_icon_state
 
 /turf/open/floor/plasteel/airless
 	initial_gas_mix = AIRLESS_ATMOS
+
 /turf/open/floor/plasteel/telecomms
 	initial_gas_mix = TCOMMS_ATMOS
+
 /turf/open/floor/plasteel/icemoon
 	initial_gas_mix = ICEMOON_DEFAULT_ATMOS
 
@@ -29,6 +30,7 @@
 	floor_tile = /obj/item/stack/tile/plasteel/dark
 /turf/open/floor/plasteel/dark/airless
 	initial_gas_mix = AIRLESS_ATMOS
+
 /turf/open/floor/plasteel/dark/telecomms
 	initial_gas_mix = TCOMMS_ATMOS
 
@@ -42,6 +44,7 @@
 
 /turf/open/floor/plasteel/mono
 	icon_state = "monotile"
+	base_icon_state = "monotile"
 /turf/open/floor/plasteel/mono/dark
 	color = COLOR_TILE_GRAY
 /turf/open/floor/plasteel/mono/white
@@ -50,43 +53,57 @@
 /turf/open/floor/plasteel/tech
 	icon = 'whitesands/icons/turf/floors/techfloor.dmi'
 	icon_state = "techfloor_gray"
+	base_icon_state = "techfloor_gray"
 	color = null
 
 /turf/open/floor/plasteel/tech/grid
 	icon_state = "techfloor_grid"
+	base_icon_state = "techfloor_grid"
 /turf/open/floor/plasteel/tech/techmaint
 	icon_state = "techmaint"
+	base_icon_state = "techmaint"
 /turf/open/floor/plasteel/patterned
 	icon_state = "tile_full"
+	base_icon_state = "tile_full"
 /turf/open/floor/plasteel/patterned/cargo_one
 	icon_state = "cargo_one_full"
+	base_icon_state = "cargo_one_full"
 /turf/open/floor/plasteel/patterned/brushed
 	icon_state = "kafel_full"
+	base_icon_state = "kafel_full"
 /turf/open/floor/plasteel/patterned/monofloor
 	icon_state = "steel_monofloor"
+	base_icon_state = "steel_monofloor"
 	color = null
 /turf/open/floor/plasteel/patterned/grid
 	icon_state = "grid"
+	base_icon_state = "grid"
 /turf/open/floor/plasteel/patterned/ridged
 	icon_state = "ridged"
+	base_icon_state = "ridged"
 
 /turf/open/floor/plasteel/showroomfloor
 	icon = 'whitesands/icons/turf/floors.dmi'
 	icon_state = "showroomfloor"
+	base_icon_state = "showroomfloor"
 
 /turf/open/floor/plasteel/showroomfloor/airless
 	initial_gas_mix = AIRLESS_ATMOS
 
 /turf/open/floor/plasteel/solarpanel
 	icon_state = "solarpanel"
+	base_icon_state = "solarpanel"
+
 /turf/open/floor/plasteel/airless/solarpanel
 	icon_state = "solarpanel"
+	base_icon_state = "solarpanel"
 
 
 /turf/open/floor/plasteel/freezer
 	color = null
 	icon = 'whitesands/icons/turf/floors.dmi'
 	icon_state = "freezer"
+	base_icon_state = "freezer"
 
 /turf/open/floor/plasteel/freezer/airless
 	initial_gas_mix = AIRLESS_ATMOS
@@ -96,35 +113,42 @@
 	name = "cold room floor"
 	icon = 'whitesands/icons/turf/floors.dmi'
 	initial_gas_mix = KITCHEN_COLDROOM_ATMOS
+
 /turf/open/floor/plasteel/kitchen_coldroom/freezerfloor //tempted to make this a subset of freezer
 	icon_state = "freezer"
+	base_icon_state = "freezer"
 
 
 /turf/open/floor/plasteel/grimy
 	color = null
 	icon = 'whitesands/icons/turf/floors.dmi'
 	icon_state = "lino"
+	base_icon_state = "lino"
 	tiled_dirt = FALSE
 	floor_tile = /obj/item/stack/tile/plasteel/grimy
 
 /turf/open/floor/plasteel/cult
 	icon_state = "cult"
+	base_icon_state = "cult"
 	color = null
 	name = "engraved floor"
 
 /turf/open/floor/plasteel/vaporwave
 	color = null
-	icon_state = "pinkblack"
 	icon = 'icons/turf/floors.dmi'
+	icon_state = "pinkblack"
+	base_icon_state = "pinkblack"
 
 /turf/open/floor/plasteel/goonplaque
-	icon_state = "plaque"
 	name = "commemorative plaque"
 	desc = "\"This is a plaque in honour of our comrades on the G4407 Stations. Hopefully TG4407 model can live up to your fame and fortune.\" Scratched in beneath that is a crude image of a meteor and a spaceman. The spaceman is laughing. The meteor is exploding."
+	icon_state = "plaque"
+	base_icon_state = "plaque"
 	tiled_dirt = FALSE
 
 /turf/open/floor/plasteel/cult/narsie_act()
 	return
+
 /turf/open/floor/plasteel/cult/airless
 	initial_gas_mix = AIRLESS_ATMOS
 
@@ -132,34 +156,50 @@
 /turf/open/floor/plasteel/stairs //considering removal
 	icon = 'icons/turf/floors.dmi'
 	icon_state = "stairs"
+	base_icon_state = "stairs"
 	tiled_dirt = FALSE
+
 /turf/open/floor/plasteel/stairs/left
 	icon_state = "stairs-l"
+	base_icon_state = "stairs-l"
+
 /turf/open/floor/plasteel/stairs/medium
 	icon_state = "stairs-m"
+	base_icon_state = "stairs-m"
+
 /turf/open/floor/plasteel/stairs/right
 	icon_state = "stairs-r"
+	base_icon_state = "stairs-r"
+
 /turf/open/floor/plasteel/stairs/old
 	icon_state = "stairs-old"
+	base_icon_state = "stairs-old"
 
 
 /turf/open/floor/plasteel/rockvault
 	icon_state = "rockvault"
+	base_icon_state = "rockvault"
 	icon = 'whitesands/icons/turf/floors/misc.dmi'
+
 /turf/open/floor/plasteel/rockvault/alien
 	icon_state = "alienvault"
+	base_icon_state = "alienvault"
+
 /turf/open/floor/plasteel/rockvault/sandstone
 	icon_state = "sandstonevault"
+	base_icon_state = "sandstonevault"
 
 
 /turf/open/floor/plasteel/elevatorshaft
 	icon = 'whitesands/icons/turf/floors.dmi'
 	icon_state = "elevatorshaft"
+	base_icon_state = "elevatorshaft"
 	color = null
 
 /turf/open/floor/plasteel/bluespace
 	icon = 'icons/turf/floors.dmi'
 	icon_state = "bluespace"
+	base_icon_state = "bluespace"
 	color = null
 
 /turf/open/floor/plasteel/sepia