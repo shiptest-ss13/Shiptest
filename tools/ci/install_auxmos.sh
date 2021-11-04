2ed49e6ce90f532df48855b2cdc69b83b96661b8
Mark Suckerberg <mark@suckerberg.ga>
2021-11-03 21:53:57 -0500

    temporary change to get CI to run

--- tools/ci/install_auxmos.sh
+++ tools/ci/install_auxmos.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 source dependencies.sh
 
-mkdir -p ~/.byond/bin
-wget -O ~/.byond/bin/libauxmos.so "https://github.com/austation/auxmos/releases/download/${AUXMOS_VERSION}/libauxmos.so"
-chmod +x ~/.byond/bin/libauxmos.so
-ldd ~/.byond/bin/libauxmos.so
+#mkdir -p ~/.byond/bin
+#wget -O ~/.byond/bin/libauxmos.so "https://github.com/austation/auxmos/releases/download/${AUXMOS_VERSION}/libauxmos.so"
+chmod +x libauxmos.so
+ldd libauxmos.so
