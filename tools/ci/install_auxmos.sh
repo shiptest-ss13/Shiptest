c894ec084b63a4e4367229b2299f674d65189307
Mark Suckerberg <mark@suckerberg.ga>
2021-11-03 22:17:51 -0500

    shrug

--- tools/ci/install_auxmos.sh
+++ tools/ci/install_auxmos.sh
@@ -5,5 +5,5 @@ source dependencies.sh
 
 #mkdir -p ~/.byond/bin
 #wget -O ~/.byond/bin/libauxmos.so "https://github.com/austation/auxmos/releases/download/${AUXMOS_VERSION}/libauxmos.so"
-chmod +x libauxmos.so
-ldd libauxmos.so
+chmod +x ./auxtools/libauxmos.so
+ldd ./auxtools/libauxmos.so
