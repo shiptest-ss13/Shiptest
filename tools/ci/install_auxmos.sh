59af6d13d73d4c06d1330b429aa5185e74a9c2d8
Mark Suckerberg <29362068+MarkSuckerberg@users.noreply.github.com>
2022-01-14 15:28:32 -0600

    Update install_auxmos.sh

--- tools/ci/install_auxmos.sh
+++ tools/ci/install_auxmos.sh
@@ -5,5 +5,5 @@ source dependencies.sh
 
 mkdir -p ~/.byond/bin
 wget -O ~/.byond/bin/libauxmos.so "https://github.com/jupyterkat/auxmos/releases/download/${AUXMOS_VERSION}/libauxmos.so"
-chmod +x ./auxtools/libauxmos.so
-ldd ./auxtools/libauxmos.so
+chmod +x ~/.byond/bin/libauxmos.so
+ldd ~/.byond/bin/libauxmos.so
