2ddf5deec270f47d44ba4c4c13d135136051ae09
ZephyrTFA <matthew@tfaluc.com>
2022-07-17 16:39:43 -0400

    agggg

--- tools/ci/check_misc.sh
+++ tools/ci/check_misc.sh
@@ -3,13 +3,3 @@ set -euo pipefail
 
 find . -name "*.php" -print0 | xargs -0 -n1 php -l
 find . -name "*.json" -not -path "*/node_modules/*" -print0 | xargs -0 python3 ./tools/json_verifier.py
-
-unset -euo pipefail
-if [ -z "$LD_LIBRARY_PATH" ]; then
-	export LD_LIBRARY_PATH=""
-fi
-set -euo pipefail
-
-source /home/runner/BYOND/byond/bin/byondsetup
-
-bash tools/build/build --ci lint tgui-test
