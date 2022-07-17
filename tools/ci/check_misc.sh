e06c6770ebf4704ab78c9a1fa43c6c6398d2ed42
ZephyrTFA <matthew@tfaluc.com>
2022-07-17 16:16:55 -0400

    do it like this

--- tools/ci/check_misc.sh
+++ tools/ci/check_misc.sh
@@ -3,3 +3,6 @@ set -euo pipefail
 
 find . -name "*.php" -print0 | xargs -0 -n1 php -l
 find . -name "*.json" -not -path "*/node_modules/*" -print0 | xargs -0 python3 ./tools/json_verifier.py
+
+source /home/runner/BYOND/byond/bin/byondsetup
+bash tools/build/build --ci lint tgui-test
