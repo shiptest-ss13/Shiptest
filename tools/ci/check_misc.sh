74f5cbfb7266f6fc2ec384ebc970041c59b05825
ZephyrTFA <matthew@tfaluc.com>
2022-07-17 16:34:58 -0400

    fix unbound

--- tools/ci/check_misc.sh
+++ tools/ci/check_misc.sh
@@ -4,5 +4,9 @@ set -euo pipefail
 find . -name "*.php" -print0 | xargs -0 -n1 php -l
 find . -name "*.json" -not -path "*/node_modules/*" -print0 | xargs -0 python3 ./tools/json_verifier.py
 
+if [ -z "$LD_LIBRARY_PATH" ]; then
+	export LD_LIBRARY_PATH=""
+fi
 source /home/runner/BYOND/byond/bin/byondsetup
+
 bash tools/build/build --ci lint tgui-test
