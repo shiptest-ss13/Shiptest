8ca8873c2ab044d46390ed365577eee3a8ed0959
Mark Suckerberg <mark@suckerberg.gay>
2023-05-23 14:15:16 -0500

    fixes linters

--- tgui/packages/tgui/interfaces/ShipSelect.js
+++ tgui/packages/tgui/interfaces/ShipSelect.js
@@ -49,7 +49,9 @@ export const ShipSelect = (props, context) => {
             buttons={
               <Button
                 content="Purchase Ship"
-                tooltip={data.purchaseBanned && "You are banned from purchasing ships."}
+                tooltip={
+                  data.purchaseBanned && 'You are banned from purchasing ships.'
+                }
                 disabled={data.purchaseBanned}
                 onClick={() => {
                   setTab(3);
