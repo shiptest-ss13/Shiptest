HolyTigerGod/Voidcrew

Fixes a bunch of harddels that are sourced from player action (#59371)
Sourced from #59118 and a cursed project I'll pr later, This pr contains a lot of harddel fixes for stuff that pops up after a player interacts with something. I'm not gonna list them all here because there's something like 60 130, check the commit log if you're curious

Oh and I moved ref tracking screaming to a separate define, and made some optimizations to the thing in general. I think that's it, this pr is a bit of a frankenstine

code/modules/mob/living/living_defense.dm
@@ -96,8 +96,9 @@
 		if(blocked)
 			return TRUE
 
-		if(thrown_item.thrownby)
-			log_combat(thrown_item.thrownby, src, "threw and hit", thrown_item)
+		var/mob/thrown_by = thrown_item.thrownby?.resolve()
+		if(thrown_by)
+			log_combat(thrown_by, src, "threw and hit", thrown_item)
 		if(nosell_hit)
 			return ..()
 		visible_message("<span class='danger'>[src] is hit by [thrown_item]!</span>", \

shiptest-ss13/Shiptest

code/modules/mob/living/living_defense.dm
@@ -93,8 +93,9 @@
 				"Your armor has softened a hit to your [parse_zone(zone)]."
 			)
 			apply_damage(I.throwforce, dtype, zone, armor)
-			if(I.thrownby)
-				log_combat(I.thrownby, src, "threw and hit", I)
+			var/mob/thrown_by = I.thrownby?.resolve()
+			if(thrown_by)
+				log_combat(thrown_by, src, "threw and hit", I)
 		else
 			return 1
 	else