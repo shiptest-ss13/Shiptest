c24d1d679f35a4d3b15381ad1db39c64249f38f5
Mark Suckerberg <mark@suckerberg.gay>
2023-12-05 22:53:23 -0600

    downgrades tests to 20.04 again

--- tools/ci/build_auxmos.sh
+++ tools/ci/build_auxmos.sh
@@ -18,6 +18,6 @@ git reset --hard FETCH_HEAD
 sudo apt-get install g++-multilib -y
 rustup target add i686-unknown-linux-gnu
 
-cargo build --release --target=i686-unknown-linux-gnu --features katmos
+cargo build --release --target=i686-unknown-linux-gnu --features "all_reaction_hooks,katmos"
 cp target/i686-unknown-linux-gnu/release/libauxmos.so  $GITHUB_WORKSPACE/libauxmos.so
 ldd $GITHUB_WORKSPACE/libauxmos.so
