16def5a7c9fe6989098c9ac2d6415430e68a1e96
Mark Suckerberg <mark@suckerberg.gay>
2023-12-08 00:25:58 -0600

    ree

--- tools/ci/build_auxmos.sh
+++ tools/ci/build_auxmos.sh
@@ -15,6 +15,8 @@ fi
 git fetch origin --depth=1 $AUXMOS_VERSION
 git reset --hard FETCH_HEAD
 
+sudo dpkg --add-architecture i386
+sudo apt-get update
 sudo apt-get install libstdc++6:i386 gcc-multilib g++-7 g++-7-multilib zlib1g:i386 libssl1.1 libssl1.1:i386 -y
 rustup target add i686-unknown-linux-gnu
 
