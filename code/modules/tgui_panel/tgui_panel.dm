HolyTigerGod/Voidcrew

refactors statpanel to use tgui API (#66971)
refactors the status panel to utilize the tgui/byond communication APIs instead of passing along href data, as well as converts the entirety of it into a datum/tgui_window

Co-authored-by: Aleksej Komarov <stylemistake@gmail.com>

code/modules/tgui_panel/tgui_panel.dm
@@ -13,9 +13,9 @@
 	var/broken = FALSE
 	var/initialized_at
 
-/datum/tgui_panel/New(client/client)
+/datum/tgui_panel/New(client/client, id)
 	src.client = client
-	window = new(client, "browseroutput")
+	window = new(client, id)
 	window.subscribe(src, .proc/on_message)
 
 /datum/tgui_panel/Del()
@@ -42,9 +42,11 @@
 	sleep(1)
 	initialized_at = world.time
 	// Perform a clean initialization
-	window.initialize(assets = list(
-		get_asset_datum(/datum/asset/simple/tgui_panel),
-	))
+	window.initialize(
+		strict_mode = TRUE,
+		assets = list(
+			get_asset_datum(/datum/asset/simple/tgui_panel),
+		))
 	window.send_asset(get_asset_datum(/datum/asset/simple/namespaced/fontawesome))
 	window.send_asset(get_asset_datum(/datum/asset/simple/namespaced/tgfont))
 	window.send_asset(get_asset_datum(/datum/asset/spritesheet/chat))

shiptest-ss13/Shiptest

code/modules/tgui_panel/tgui_panel.dm
@@ -13,9 +13,9 @@
 	var/broken = FALSE
 	var/initialized_at
 
-/datum/tgui_panel/New(client/client)
+/datum/tgui_panel/New(client/client, id)
 	src.client = client
-	window = new(client, "browseroutput")
+	window = new(client, id)
 	window.subscribe(src, PROC_REF(on_message))
 
 /datum/tgui_panel/Del()
@@ -42,10 +42,12 @@
 	sleep(1)
 	initialized_at = world.time
 	// Perform a clean initialization
-	window.initialize(inline_assets = list(
-		get_asset_datum(/datum/asset/simple/tgui_common),
-		get_asset_datum(/datum/asset/simple/tgui_panel),
-	))
+	window.initialize(
+		strict_mode = TRUE,
+		inline_assets = list(
+			get_asset_datum(/datum/asset/simple/tgui_common),
+			get_asset_datum(/datum/asset/simple/tgui_panel),
+		))
 	window.send_asset(get_asset_datum(/datum/asset/simple/namespaced/fontawesome))
 	window.send_asset(get_asset_datum(/datum/asset/spritesheet/chat))
 	request_telemetry()