bb41552da6006a91dfc232339fdef4079368ceb2
FalloutFalcon <falloutfalconplays@gmail.com>
2025-05-05 11:01:18 -0500

    yarn install + yarn dlx @yarnpkg/sdks vscode linting works.

--- dependencies.sh
+++ dependencies.sh
@@ -17,7 +17,7 @@ export RUST_G_VERSION=3.6.0
 export NODE_VERSION_LTS=22.11.0
 
 # SpacemanDMM git tag
-export SPACEMAN_DMM_VERSION=suite-1.8
+export SPACEMAN_DMM_VERSION=suite-1.9
 
 # Python version for mapmerge and other tools
 export PYTHON_VERSION=3.9.0
