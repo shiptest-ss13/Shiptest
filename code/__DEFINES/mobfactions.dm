shiptest-ss13/Shiptest

commit 633492c6a52931b4b5326a6cae6d185178c65717

more randomization for simple humans (#5096)
<!-- Write **BELOW** The Headers and **ABOVE** The comments else it may
not be viewable. -->
<!-- You can view Contributing.MD for a detailed description of the pull
request process. -->

## About The Pull Request
Adds both species and random seed randomization of simple humans 
<img width="586" height="438" alt="image"
src="https://github.com/user-attachments/assets/b1b14d79-189d-4277-9316-cdcf72fd26eb"
/>

<!-- Describe The Pull Request. Please be sure every change is
documented or this can delay review and even discourage maintainers from
merging your PR! -->

## Why It's Good For The Game
No more 1 billion generic white male frontiersmen
<!-- Please add a short description of why you think these changes would
benefit the game. If you can't justify it in words, it might not be
worth adding. -->

## Changelog

:cl:
add: The appearance of simple humans is now much more varied
/:cl:

<!-- Both :cl:'s are required for the changelog to work! You can put
your name to the right of the first :cl: if you want to overwrite your
GitHub username as author ingame. -->
<!-- You can use multiple of the same prefix (they're only used for the
icon ingame) and delete the unneeded ones. Despite some of the tags,
changelogs should generally represent how a player might be affected by
the changes rather than a summary of the PR's contents. -->

code/datums/elements/death_drops.dm
@@ -14,9 +14,8 @@
 	if(!isliving(target))
 		return ELEMENT_INCOMPATIBLE
 	if(!loot)
-		stack_trace("death drops element added to [target] with NO LOOT")
-	if(!src.loot)
-		src.loot = loot.Copy()
+		stack_trace("[type] added to [target] with NO LOOT.")
+	src.loot = loot
 	RegisterSignal(target, COMSIG_MOB_DEATH, PROC_REF(on_death))
 
 /datum/element/death_drops/Detach(datum/target)
@@ -26,9 +25,32 @@
 ///signal called by the stat of the target changing
 /datum/element/death_drops/proc/on_death(mob/living/target, gibbed)
 	SIGNAL_HANDLER
+	var/atom/loot_loc = target.drop_location()
 	for(var/thing_to_spawn in loot)
-		if(loot[thing_to_spawn]) //If this is an assoc list, use the value of that to get the right amount
-			for(var/index in 1 to loot[thing_to_spawn])
-				new thing_to_spawn(target.drop_location())
-		else
-			new thing_to_spawn(target.drop_location())
+		for(var/i in 1 to (loot[thing_to_spawn] || 1))
+			create_loot(thing_to_spawn, loot_loc, target, gibbed)
+
+/// Handles creating the loots
+/datum/element/death_drops/proc/create_loot(typepath, atom/loot_loc, mob/living/dead, gibbed)
+	if(ispath(typepath, /obj/effect/mob_spawn/human/corpse))
+		handle_corpse(typepath, loot_loc, dead, gibbed)
+		return
+
+	new typepath(loot_loc)
+
+/// Handles snowflake case of mob corpses
+/datum/element/death_drops/proc/handle_corpse(typepath, atom/loot_loc, mob/living/dead, gibbed)
+	var/obj/effect/mob_spawn/human/corpse/spawner = new typepath(loot_loc)
+	/*
+	var/mob/living/body = spawner.create()
+	// done before the gib check so the bodyparts will be damaged
+	body.adjustBruteLoss(dead.getBruteLoss())
+	body.adjustFireLoss(dead.getFireLoss())
+	// if gibbed, dispose of the body
+	if(gibbed)
+		body.gib(DROP_ALL_REMAINS)
+		return
+	// otherwise continue with the rest of the damage types
+	body.adjustToxLoss(dead.getToxLoss())
+	body.adjustOxyLoss(dead.getOxyLoss())
+	*/

code/modules/mob/living/simple_animal/hostile/human/human.dm
@@ -41,9 +41,9 @@
 	/// If we use stuff from dynamic human icon generation for loot
 	var/human_loot = TRUE
 	/// Path of the mob spawner we base the mob's visuals off of.
-	var/mob_spawner
+	var/obj/effect/mob_spawn/human/mob_spawner
 	/// Path of the species we base the mob's visuals off of.
-	var/species_spawner = /datum/species/human
+	var/datum/species/mob_species
 	/// Path of the right hand held item we give to the mob's visuals.
 	var/obj/r_hand
 	/// THE DEFAULT HAND (Required if you want them to wield it). Path of the left hand held item we give to the mob's visuals.
@@ -57,7 +57,17 @@
 /mob/living/simple_animal/hostile/human/Initialize(mapload)
 	. = ..()
 	if(mob_spawner)
-		apply_dynamic_human_appearance(src, species_path = species_spawner, mob_spawn_path = mob_spawner, r_hand = r_hand, l_hand = l_hand)
+		if(!mob_species)
+			mob_species = pick_weight(list(
+					/datum/species/lizard = 28,
+					/datum/species/human = 22,
+					/datum/species/ipc = 20,
+					/datum/species/elzuose = 20,
+					/datum/species/moth = 5,
+					/datum/species/spider = 3
+				)
+			)
+		apply_dynamic_human_appearance(src, species_path = mob_species, mob_spawn_path = mob_spawner, r_hand = r_hand, l_hand = l_hand, seed = rand(1,3))
 		if(ispath(r_hand,/obj/item/gun))
 			var/obj/item/gun/our_gun = r_hand
 			spread = our_gun.spread
@@ -66,10 +76,8 @@
 			spread = our_gun.spread
 
 	if(ispath(armor_base, /obj/item/clothing))
-		//sigh. if only we could get the initial() value of list vars
-		var/obj/item/clothing/instance = new armor_base()
-		armor = instance.armor
-		qdel(instance)
+		//Reconstructing the armor from the list is definitly cheaper then creating the whole armor
+		armor = getArmor(arglist(armor_base::armor))
 
 /mob/living/simple_animal/hostile/human/drop_loot()
 	. = ..()
@@ -78,7 +86,7 @@
 	if(!human_loot)
 		return
 	if(mob_spawner)
-		new mob_spawner(loc)
+		new mob_spawner(loc, mob_species)
 	if(r_hand && weapon_drop_chance)
 		if(prob(weapon_drop_chance))
 			var/obj/item/gun/ballistic/cosmetic_damage = new r_hand(loc)

code/__HELPERS/dynamic_human_icon_gen.dm
@@ -2,16 +2,16 @@
 GLOBAL_LIST_EMPTY(dynamic_human_appearances)
 
 /// Creates a human with the given parameters and returns an appearance of it
-/proc/get_dynamic_human_appearance(outfit_path, species_path = /datum/species/human, mob_spawn_path, r_hand, l_hand, bloody_slots = NONE, animated = TRUE)
+/proc/get_dynamic_human_appearance(outfit_path, species_path = /datum/species/human, mob_spawn_path, r_hand, l_hand, bloody_slots = NONE, seed = 1)
 	if(!species_path)
 		return FALSE
 	if(!ispath(species_path))
 		stack_trace("Attempted to call get_dynamic_human_appearance() with an instantiated species_path. Pass the species datum typepath instead.")
 		return FALSE
-	var/arg_string = "[outfit_path]_[species_path]_[mob_spawn_path]_[l_hand]_[r_hand]_[bloody_slots]_[rand(1,5)]"
+	var/arg_string = "[outfit_path]_[species_path]_[mob_spawn_path]_[l_hand]_[r_hand]_[bloody_slots]_[seed]"
 	if(GLOB.dynamic_human_appearances[arg_string]) //if already exists in our cache, just return that
 		return GLOB.dynamic_human_appearances[arg_string]
-	var/mob/living/carbon/human/dummy/consistent/dummy = new()
+	var/mob/living/carbon/human/dummy/dummy = new()
 	dummy.set_species(species_path)
 	dummy.stat = CONSCIOUS //He needs to be alive or he has no eyes. Scary
 	dummy.underwear = "Nude"
@@ -50,7 +50,7 @@ GLOBAL_LIST_EMPTY(dynamic_human_appearances)
 	return output
 
 ///This exists to apply the icons async, as that cannot be done in Initialize because of possible sleeps.
-/proc/apply_dynamic_human_appearance(atom/target, outfit_path, species_path = /datum/species/human, mob_spawn_path, r_hand, l_hand, bloody_slots = NONE)
+/proc/apply_dynamic_human_appearance(atom/target, outfit_path, species_path = /datum/species/human, mob_spawn_path, r_hand, l_hand, bloody_slots = NONE, seed = 1)
 	INVOKE_ASYNC(GLOBAL_PROC, GLOBAL_PROC_REF(set_dynamic_human_appearance), args)
 
 ///This proc gets an argument of a target and runs

code/modules/awaymissions/corpse.dm
@@ -177,7 +177,9 @@
 
 	var/list/outfit_override
 
-/obj/effect/mob_spawn/human/Initialize()
+/obj/effect/mob_spawn/human/Initialize(mapload, species)
+	if(species)
+		mob_species = species
 	if(ispath(outfit))
 		outfit = new outfit()
 	if(!outfit)

code/modules/ruins/icemoonruin_code/ice_lodge.dm
@@ -15,7 +15,7 @@
 	mob_spawner = /obj/effect/mob_spawn/human/corpse/frontier/ranged/officer/internals/ice_lodge
 	armor_base = /obj/item/clothing/suit/armor/vest/marine/frontier
 	weapon_drop_chance = 0
-	species_spawner = /datum/species/vox
+	mob_species = /datum/species/vox
 
 /obj/effect/mob_spawn/human/corpse/frontier/ranged/officer/internals/ice_lodge
 	mob_species = /datum/species/vox

commit a2ae705b9e4850c45d51625515e482cfbd6901ee

fix simple hostile human armor (#5361)
<!-- Write **BELOW** The Headers and **ABOVE** The comments else it may
not be viewable. -->
<!-- You can view Contributing.MD for a detailed description of the pull
request process. -->

## About The Pull Request
broken in https://github.com/shiptest-ss13/Shiptest/pull/5096

also some removal of unused var and some define sanity (I need to
deprecate the old faction defines.)
<!-- Describe The Pull Request. Please be sure every change is
documented or this can delay review and even discourage maintainers from
merging your PR! -->

<!-- Please add a short description of why you think these changes would
benefit the game. If you can't justify it in words, it might not be
worth adding. -->

## Changelog

:cl:
fix: simple hostile humans should now have armor again
/:cl:

<!-- Both :cl:'s are required for the changelog to work! You can put
your name to the right of the first :cl: if you want to overwrite your
GitHub username as author ingame. -->
<!-- You can use multiple of the same prefix (they're only used for the
icon ingame) and delete the unneeded ones. Despite some of the tags,
changelogs should generally represent how a player might be affected by
the changes rather than a summary of the PR's contents. -->

code/__DEFINES/mobfactions.dm
@@ -11,7 +11,7 @@
 // anything with these factions should be hostile to the average player.
 #define FACTION_ANTAG_SYNDICATE "Syndicate"
 #define FACTION_ANTAG_FRONTIERSMEN "Frontiersmen"
-#define FACTION_ANTAG_HERMITS "Hermit"
+#define FACTION_ANTAG_HERMITS "hermit"
 
 //Player Factions
 #define FACTION_PLAYER_SYNDICATE "playerSyndicate"

code/datums/elements/death_drops.dm
@@ -32,15 +32,16 @@
 
 /// Handles creating the loots
 /datum/element/death_drops/proc/create_loot(typepath, atom/loot_loc, mob/living/dead, gibbed)
-	if(ispath(typepath, /obj/effect/mob_spawn/human/corpse))
-		handle_corpse(typepath, loot_loc, dead, gibbed)
-		return
+	//if(ispath(typepath, /obj/effect/mob_spawn/human/corpse))
+	//	handle_corpse(typepath, loot_loc, dead, gibbed)
+	//	return
 
 	new typepath(loot_loc)
 
 /// Handles snowflake case of mob corpses
 /datum/element/death_drops/proc/handle_corpse(typepath, atom/loot_loc, mob/living/dead, gibbed)
-	var/obj/effect/mob_spawn/human/corpse/spawner = new typepath(loot_loc)
+	new typepath(loot_loc)
+	//var/obj/effect/mob_spawn/human/corpse/spawner = new typepath(loot_loc)
 	/*
 	var/mob/living/body = spawner.create()
 	// done before the gib check so the bodyparts will be damaged

code/modules/mining/ore_veins.dm
@@ -42,7 +42,7 @@ GLOBAL_LIST_EMPTY(ore_veins)
 		/mob/living/simple_animal/hostile/asteroid/brimdemon = 20,
 		)
 	var/spawn_text = "emerges from"
-	var/faction = list("hostile","mining")
+	var/faction = list(FACTION_HOSTILE, FACTION_MINING)
 	var/spawn_sound = list('sound/effects/break_stone.ogg')
 	var/spawner_type = /datum/component/spawner
 

code/modules/mob/living/basic/vermin/cockroach.dm
@@ -22,7 +22,7 @@
 	speak_emote = list("chitters")
 
 	basic_mob_flags = DEL_ON_DEATH
-	faction = list("hostile")
+	faction = list(FACTION_HOSTILE)
 
 	unsuitable_atmos_damage = 0
 	minimum_survivable_temperature = 270

code/modules/mob/living/simple_animal/friendly/snake.dm
@@ -29,7 +29,7 @@
 	response_disarm_simple = "shoo"
 	response_harm_continuous = "steps on"
 	response_harm_simple = "step on"
-	faction = list("hostile")
+	faction = list(FACTION_HOSTILE)
 	ventcrawler = VENTCRAWLER_ALWAYS
 	density = FALSE
 	pass_flags = PASSTABLE | PASSMOB

code/modules/mob/living/simple_animal/hostile/bees.dm
@@ -36,7 +36,7 @@
 	response_harm_simple = "squash"
 	maxHealth = 10
 	health = 10
-	faction = list("hostile")
+	faction = list(FACTION_HOSTILE)
 	move_to_delay = 0
 	obj_damage = 0
 	ventcrawler = VENTCRAWLER_ALWAYS

code/modules/mob/living/simple_animal/hostile/hostile.dm
@@ -1,5 +1,5 @@
 /mob/living/simple_animal/hostile
-	faction = list("hostile")
+	faction = list(FACTION_HOSTILE)
 	stop_automated_movement_when_pulled = 0
 	obj_damage = 40
 	environment_smash = ENVIRONMENT_SMASH_STRUCTURES //Bitflags. Set to ENVIRONMENT_SMASH_STRUCTURES to break closets,tables,racks, etc; ENVIRONMENT_SMASH_WALLS for walls; ENVIRONMENT_SMASH_RWALLS for rwalls

code/modules/mob/living/simple_animal/hostile/human/human.dm
@@ -36,7 +36,7 @@
 
 	footstep_type = FOOTSTEP_MOB_SHOE
 
-	faction = list("hermit")
+	faction = list(FACTION_ANTAG_HERMITS)
 
 	/// If we use stuff from dynamic human icon generation for loot
 	var/human_loot = TRUE
@@ -76,8 +76,10 @@
 			spread = our_gun.spread
 
 	if(ispath(armor_base, /obj/item/clothing))
-		//Reconstructing the armor from the list is definitly cheaper then creating the whole armor
-		armor = getArmor(arglist(armor_base::armor))
+		//sigh. if only we could get the initial() value of list vars
+		var/obj/item/clothing/instance = new armor_base()
+		armor = instance.armor
+		qdel(instance)
 
 /mob/living/simple_animal/hostile/human/drop_loot()
 	. = ..()

code/modules/mob/living/simple_animal/hostile/retaliate/bat.dm
@@ -22,7 +22,7 @@
 	attack_verb_simple = "bite"
 	butcher_results = list(/obj/item/food/meat/slab = 1)
 	pass_flags = PASSTABLE
-	faction = list("hostile")
+	faction = list(FACTION_HOSTILE)
 	attack_sound = 'sound/weapons/bite.ogg'
 	obj_damage = 0
 	environment_smash = ENVIRONMENT_SMASH_NONE

code/modules/mob/living/simple_animal/hostile/retaliate/frog.dm
@@ -23,7 +23,7 @@
 	response_harm_simple = "splat"
 	density = FALSE
 	ventcrawler = VENTCRAWLER_ALWAYS
-	faction = list("hostile")
+	faction = list(FACTION_HOSTILE)
 	attack_sound = 'sound/effects/reee.ogg'
 	butcher_results = list(/obj/item/food/nugget = 1)
 	pass_flags = PASSTABLE | PASSGRILLE | PASSMOB

code/modules/mob/living/simple_animal/hostile/tree.dm
@@ -14,7 +14,7 @@
 	response_help_simple = "brush"
 	response_disarm_continuous = "pushes"
 	response_disarm_simple = "push"
-	faction = list("hostile")
+	faction = list(FACTION_HOSTILE)
 	speed = 1
 	maxHealth = 250
 	health = 250

code/modules/mob/living/simple_animal/hostile/venus_human_trap.dm
@@ -99,7 +99,7 @@
 	atmos_requirements = IMMUNE_ATMOS_REQS
 	unsuitable_atmos_damage = 0
 	lighting_alpha = LIGHTING_PLANE_ALPHA_MOSTLY_INVISIBLE
-	faction = list("hostile","vines","plants")
+	faction = list(FACTION_HOSTILE,"vines","plants")
 	initial_language_holder = /datum/language_holder/venus
 	del_on_death = TRUE
 	/// A list of all the plant's vines

code/modules/mob_spawner/spawner.dm
@@ -12,7 +12,7 @@
 	var/spawn_time = 30 SECONDS
 	var/mob_types = list(/mob/living/simple_animal/hostile/carp)
 	var/spawn_text = "emerges from"
-	var/faction = list("hostile")
+	var/faction = list(FACTION_HOSTILE)
 	var/spawn_sound = list('sound/effects/break_stone.ogg')
 	var/spawner_type = /datum/component/spawner
 	var/spawn_distance_min = 1

code/modules/ruins/wasteplanet_ruin_code/facility.dm
@@ -26,14 +26,14 @@
 	name = "reprogrammed hivebot"
 	desc = "A towering scrap-clad automaton. It appears to have been haphazardly rewired and rebranded, with a logo you don't recognise printed on its outer plating."
 	aggro_quips = list("C#$ 7-/<!!","$&&#># 7@<@1!!","D$END#&TERI$@$S!!","/C$$&ITY$>WI&HINA^@!!","I$$RAR>$@$<T#$IN$AO$!!","$ER@$A#E #OS/E!!","$@P$#A$#&$#^#$EE/TT$@^N/@!!","A#SES<>$$ ENGAG$!!","$#$CL$^IM$AR!!","&#OR$/$NCL$@IM!!")
-	faction = list("hermit")
+	faction = list(FACTION_ANTAG_HERMITS)
 
 /mob/living/simple_animal/hostile/abandoned_minebot/reprogrammed
 	name = "reprogrammed minebot"
 	desc = "A rusted minebot fitted with antiquated mining and salvage tools. It appears to have been haphazardly rewired and rebranded, with a logo you don't recognise printed on its outer plating."
-	faction = list("hermit")
+	faction = list(FACTION_ANTAG_HERMITS)
 
 /mob/living/simple_animal/hostile/viscerator/reprogrammed
 	name = "reprogrammed viscerator"
 	desc = "A small, twin-bladed machine capable of inflicting very deadly lacerations. It appears to have been haphazardly rewired and rebranded, with a logo you don't recognise printed on its outer plating."
-	faction = list("hermit")
+	faction = list(FACTION_ANTAG_HERMITS)