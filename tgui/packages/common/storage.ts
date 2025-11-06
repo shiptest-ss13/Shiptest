f77529343c2fbabc986f802aa2e0ec672545f630
harry <me@harryob.live>
2025-11-06 10:07:33 +0000

    moves clientside storage to an iframe instead of byondstorage (#93044)
    this converts the dependency on byondstorage, which is laggy (due to
    being a large json file written to disk every 10 seconds), to using
    indexeddb, like we did prior to 516. this is achieved by using an iframe
    to give us a persistent origin, as the web is evil and has invented
    same-origin policy
    https://developer.mozilla.org/en-US/docs/Web/Security/Same-origin_policy.
    
    this also hosts the iframe page on the github pages site for a secondary
    repository (see tgstation/byond-client-storage) so it works OOTB without
    requiring server operators to setup the webroot cdn (which i don't
    believe is configured on the tg servers at the moment)
    
    however, if a server is using the webroot cdn, it will use that instead
    of github pages
    
    you could also host the iframe.html page on a separate host from your
    cdn or github pages if you wanted to
    
    if we can't access the configured cdn at all, it failovers to use
    byondstorage anyway, if the internet stops working and you still want
    your chat history to save, i guess
    
    saving this enormous json file is laggy, and this solution would fix
    https://github.com/tgstation/tgstation/issues/89988 and fix
    https://github.com/tgstation/tgstation/issues/92035
    
    i am open to other solutions, but this does seem to require the least
    amount of external dependencies of posed solutions
    
    :cl:
    fix: you should experience less stutters every 10 seconds
    server: server operators can now configure an alternative storage domain
    for clientside data storage, read the example configuration for more
    /:cl:
    
    ---------
    
    Co-authored-by: harryob <55142896+harryob@users.noreply.github.com>

--- tgui/packages/common/storage.ts
+++ tgui/packages/common/storage.ts
@@ -8,19 +8,12 @@
 
 export const IMPL_MEMORY = 0;
 export const IMPL_HUB_STORAGE = 1;
-export const IMPL_INDEXED_DB = 2;
+export const IMPL_IFRAME_INDEXED_DB = 2;
 
 type StorageImplementation =
   | typeof IMPL_MEMORY
   | typeof IMPL_HUB_STORAGE
-  | typeof IMPL_INDEXED_DB;
-
-const INDEXED_DB_VERSION = 1;
-const INDEXED_DB_NAME = 'tgui';
-const INDEXED_DB_STORE_NAME = 'storage-v1';
-
-const READ_ONLY = 'readonly';
-const READ_WRITE = 'readwrite';
+  | typeof IMPL_IFRAME_INDEXED_DB;
 
 type StorageBackend = {
   impl: StorageImplementation;
@@ -103,66 +96,77 @@ class HubStorageBackend implements StorageBackend {
   }
 }
 
-class IndexedDbBackend implements StorageBackend {
+class IFrameIndexedDbBackend implements StorageBackend {
   public impl: StorageImplementation;
-  public dbPromise: Promise<IDBDatabase>;
+
+  private documentElement: HTMLIFrameElement;
+  private iframeWindow: Window;
 
   constructor() {
-    this.impl = IMPL_INDEXED_DB;
-    this.dbPromise = new Promise((resolve, reject) => {
-      const indexedDB = window.indexedDB || window.msIndexedDB;
-      const req = indexedDB.open(INDEXED_DB_NAME, INDEXED_DB_VERSION);
-      req.onupgradeneeded = () => {
-        try {
-          req.result.createObjectStore(INDEXED_DB_STORE_NAME);
-        } catch (err) {
-          reject(
-            new Error(
-              'Failed to upgrade IDB: ' +
-                (err instanceof Error ? err.message : String(err)),
-            ),
-          );
-        }
-      };
-      req.onsuccess = () => resolve(req.result);
-      req.onerror = () => {
-        reject(new Error('Failed to open IDB: ' + req.error));
-      };
-    });
+    this.impl = IMPL_IFRAME_INDEXED_DB;
   }
 
-  private async getStore(mode: IDBTransactionMode): Promise<IDBObjectStore> {
-    const db = await this.dbPromise;
-    return db
-      .transaction(INDEXED_DB_STORE_NAME, mode)
-      .objectStore(INDEXED_DB_STORE_NAME);
+  async ready(): Promise<boolean | null> {
+    const iframe = document.createElement('iframe');
+    iframe.style.display = 'none';
+    iframe.src = Byond.storageCdn;
+
+    const completePromise: Promise<boolean> = new Promise((resolve) => {
+      iframe.onload = () => resolve(true);
+    });
+
+    this.documentElement = document.body.appendChild(iframe);
+    if (!this.documentElement.contentWindow) {
+      return new Promise((res) => res(false));
+    }
+
+    this.iframeWindow = this.documentElement.contentWindow;
+
+    return completePromise;
   }
 
   async get(key: string): Promise<any> {
-    const store = await this.getStore(READ_ONLY);
-    return new Promise((resolve, reject) => {
-      const req = store.get(key);
-      req.onsuccess = () => resolve(req.result);
-      req.onerror = () => reject(req.error);
+    const promise = new Promise((resolve) => {
+      window.addEventListener('message', (message) => {
+        if (message.data.key && message.data.key === key) {
+          resolve(message.data.value);
+        }
+      });
     });
+
+    this.iframeWindow.postMessage({ type: 'get', key: key }, '*');
+    return promise;
   }
 
   async set(key: string, value: any): Promise<void> {
-    // NOTE: We deliberately make this operation transactionless
-    const store = await this.getStore(READ_WRITE);
-    store.put(value, key);
+    this.iframeWindow.postMessage({ type: 'set', key: key, value: value }, '*');
   }
 
   async remove(key: string): Promise<void> {
-    // NOTE: We deliberately make this operation transactionless
-    const store = await this.getStore(READ_WRITE);
-    store.delete(key);
+    this.iframeWindow.postMessage({ type: 'remove', key: key }, '*');
   }
 
   async clear(): Promise<void> {
-    // NOTE: We deliberately make this operation transactionless
-    const store = await this.getStore(READ_WRITE);
-    store.clear();
+    this.iframeWindow.postMessage({ type: 'clear' }, '*');
+  }
+
+  async ping(): Promise<boolean> {
+    const promise: Promise<boolean> = new Promise((resolve) => {
+      window.addEventListener('message', (message) => {
+        if (message.data === true) {
+          resolve(true);
+        }
+      });
+
+      setTimeout(() => resolve(false), 100);
+    });
+
+    this.iframeWindow.postMessage({ type: 'ping' }, '*');
+    return promise;
+  }
+
+  async destroy(): Promise<void> {
+    document.body.removeChild(this.documentElement);
   }
 }
 
@@ -176,16 +180,52 @@ class StorageProxy implements StorageBackend {
 
   constructor() {
     this.backendPromise = (async () => {
-      if (!Byond.TRIDENT && testHubStorage()) {
+      if (!Byond.TRIDENT && Byond.storageCdn && !window.hubStorage) {
+        const iframe = new IFrameIndexedDbBackend();
+        await iframe.ready();
+
+        if ((await iframe.ping()) === true) {
+          if (await iframe.get('byondstorage-migrated')) return iframe;
+
+          Byond.winset(null, 'browser-options', '+byondstorage');
+
+          await new Promise<void>((resolve) => {
+            document.addEventListener('byondstorageupdated', async () => {
+              setTimeout(() => {
+                const hub = new HubStorageBackend();
+
+                for (const setting of ['panel-settings', 'chat-state', 'chat-messages']) {
+                  hub
+                    .get(setting)
+                    .then((settings) => iframe.set(setting, settings));
+                }
+
+                iframe.set('byondstorage-migrated', true);
+                Byond.winset(null, 'browser-options', '-byondstorage');
+
+                resolve();
+              }, 1);
+            });
+          });
+
+          return iframe;
+        }
+
+        iframe.destroy();
+
+        if (!testHubStorage()) {
+          Byond.winset(null, 'browser-options', '+byondstorage');
+
+          return new Promise((resolve) => {
+            const listener = () => {
+              document.removeEventListener('byondstorageupdated', listener);
+              resolve(new HubStorageBackend());
+            };
+
+            document.addEventListener('byondstorageupdated', listener);
+          });
+        }
         return new HubStorageBackend();
-      }
-      // TODO: Remove with 516
-      if (testIndexedDb()) {
-        try {
-          const backend = new IndexedDbBackend();
-          await backend.dbPromise;
-          return backend;
-        } catch {}
       }
       console.warn(
         'No supported storage backend found. Using in-memory storage.',
