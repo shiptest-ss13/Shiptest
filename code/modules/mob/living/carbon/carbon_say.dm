shiptest-ss13/Shiptest

commit 20841e2f6addd7822b4668585cb3ff61e7a13c3b

Adds asthma (#5502)
<!-- Write **BELOW** The Headers and **ABOVE** The comments else it may
not be viewable. -->
<!-- You can view Contributing.MD for a detailed description of the pull
request process. -->

## About The Pull Request

<!-- Describe The Pull Request. Please be sure every change is
documented or this can delay review and even discourage maintainers from
merging your PR! -->

Largely ported from tgstation/tgstation#92747 with some adjustments to
make it function within this codebase. More details can be found on the
original PR.

Other additions:
- Smoking a cigarette now causes secondhand smoke that affects anyone
nearby.
- Histamine and certain irritating gases like SO2 and HCl can cause lung
inflammation even in non-asthmatics.
- Salbutamol now only heals suffocation damage if the patient is
receiving enough oxygen/plasma/etc to breathe, but significantly
improves the lungs' ability to receive enough in the first place.

## Why It's Good For The Game

There aren't many interesting quirks in the game anymore, so I felt like
adding one.

## Changelog

:cl: nikothedude, sapphicoverload, swanni
add: Added asthma
add: Added inhalers, a new method of applying reagents
add: Oxygen deprivation medical kits now contain salbutamol in inhaler
form instead of an injector
add: Histamine and certain gases like SO2 and HCl can cause lung
inflammation, even in non-asthmatics
balance: Salbutamol only heals suffocation damage if the affected mob is
currently receiving enough oxygen/plasma/etc to breathe, but
significantly increases their ability to receive enough in the first
place
fix: Fixed reagent duplication from smoke
/:cl:

<!-- Both :cl:'s are required for the changelog to work! You can put
your name to the right of the first :cl: if you want to overwrite your
GitHub username as author ingame. -->
<!-- You can use multiple of the same prefix (they're only used for the
icon ingame) and delete the unneeded ones. Despite some of the tags,

code/modules/mob/living/carbon/carbon_say.dm
@@ -14,6 +14,12 @@
 		return 0
 	return ..()
 
+/mob/living/carbon/get_message_mods(message, list/mods)
+	var/obj/item/organ/lungs/our_lungs = getorganslot(ORGAN_SLOT_LUNGS)
+	if(our_lungs?.received_pressure_mult < 0.5 && !mods[WHISPER_MODE] && !HAS_TRAIT(src, TRAIT_NOBREATH))
+		mods[WHISPER_MODE] = MODE_WHISPER
+	return ..(message, mods)
+
 /mob/living/carbon/can_sign(message)
 	if(usable_hands <= 0)
 		to_chat(src, span_warning((num_hands > 0) ? "You can't feel your hands!" : "You can't sign with no hands!"))

code/__DEFINES/alerts.dm
@@ -1,0 +1,14 @@
+/** Atmos related */
+#define ALERT_TOO_MUCH_OXYGEN "too_much_oxy"
+#define ALERT_NOT_ENOUGH_OXYGEN "not_enough_oxy"
+#define ALERT_TOO_MUCH_PLASMA "too_much_plas"
+#define ALERT_NOT_ENOUGH_PLASMA "not_enough_plas"
+#define ALERT_TOO_MUCH_CO2 "too_much_co2"
+#define ALERT_NOT_ENOUGH_CO2 "not_enough_co2"
+#define ALERT_TOO_MUCH_N2O "too_much_n2o"
+#define ALERT_NOT_ENOUGH_N2O "not_enough_n2o"
+#define ALERT_TOO_MUCH_NITRO "too_much_nitro"
+#define ALERT_NOT_ENOUGH_NITRO "not_enough_nitro"
+
+#define ALERT_BRONCHODILATION "bronchodilated"
+#define ALERT_BRONCHOCONSTRICTION "bronchoconstricted"

code/__DEFINES/atmospherics.dm
@@ -349,8 +349,12 @@
 #define GAS_METHANE "methane"
 #define GAS_AMMONIA "ammonia"
 
+/// Used to determine whether a canister should alert admins if opened
 #define GAS_FLAG_DANGEROUS (1<<0)
+/// Do not use this unless you absolutely have to
 #define GAS_FLAG_BREATH_PROC (1<<1)
+/// Can cause lung irritation
+#define GAS_FLAG_IRRITANT (1<<2)
 
 // odors
 #define GAS_ODOR_CHEMICAL list(\

code/__DEFINES/chat.dm
@@ -41,3 +41,5 @@
 #define custom_boxed_message(classes, str) ("<div class='boxed_message " + classes + "'>" + str + "</div>")
 /// Makes a fieldset with a neaty styled name. Can apply additional classes.
 #define fieldset_block(title, content, classes) ("<fieldset class='fieldset " + classes + "'><legend class='fieldset_legend'>" + title + "</legend>" + content + "</fieldset>")
+/// Helper which creates a chat message which may have a tooltip in some contexts, but not others.
+#define conditional_tooltip(normal_text, tooltip_text, condition) ((condition) ? (span_tooltip(tooltip_text, normal_text)) : (normal_text))

code/__DEFINES/dcs/signals/signals.dm
@@ -838,3 +838,9 @@
 #define COMSIG_MOVETYPE_FLAG_ENABLED "movetype_flag_enabled"
 /// From base of datum/element/movetype_handler/on_movement_type_trait_loss: (flag)
 #define COMSIG_MOVETYPE_FLAG_DISABLED "movetype_flag_disabled"
+
+//from base of [/obj/effect/particle_effect/fluid/smoke/proc/smoke_mob]: (seconds_per_tick)
+#define COMSIG_CARBON_EXPOSED_TO_SMOKE "carbon_exposed_to_smoke"
+
+//from base of [/obj/item/organ/lungs/check_breath]: (datum/gas_mixture/breath, received_pressure_mult)
+#define COMSIG_CARBON_INHALED_GAS "carbon_inhaled_gas"

code/__DEFINES/diseases.dm
@@ -10,6 +10,8 @@
 #define CURABLE (1<<0)
 #define CAN_CARRY (1<<1)
 #define CAN_RESIST (1<<2)
+/// Instead of instantly curing the disease, cures will simply reduce the stage
+#define INCREMENTAL_CURE (1<<4)
 
 //Spread Flags
 #define DISEASE_SPREAD_SPECIAL (1<<0)

code/__DEFINES/reagents.dm
@@ -17,12 +17,12 @@
 #define OPENCONTAINER (REFILLABLE | DRAINABLE | TRANSPARENT)
 
 
-#define TOUCH 1	// splashing
-#define INGEST 2	// ingestion
-#define VAPOR 3	// foam, spray, blob attack
-#define PATCH 4	// patches
-#define INJECT 5	// injection
-#define SMOKE 6 //smoking
+#define TOUCH (1<<0) // splashing
+#define INGEST (1<<1) // ingestion
+#define VAPOR (1<<2) // foam, spray, blob attack
+#define PATCH (1<<3) // patches
+#define INJECT (1<<4) // injection
+#define INHALE (1<<5) // smoking, inhalers
 
 //defines passed through to the on_reagent_change proc
 #define DEL_REAGENT 1	// reagent deleted (fully cleared)

code/__DEFINES/traits/traits.dm
@@ -141,6 +141,10 @@ Remember to update _globalvars/traits.dm if you're adding/removing/renaming trai
 #define TRAIT_CRITICAL_CONDITION "critical-condition"
 /// Doesn't miss attacks
 #define TRAIT_PERFECT_ATTACKER "perfect_attacker"
+/// Is in stasis
+#define TRAIT_STASIS "stasis"
+/// This mob is asthmatic, and has much more severe reactions to any irritants present in the air
+#define TRAIT_ASTHMATIC "asthmatic"
 #define TRAIT_BLIND "blind"
 #define TRAIT_DEAF "deaf"
 #define TRAIT_MUTE "mute"

commit 99aa49431bb6a9e6d14c218a8fdf70b27f74bfdf

Alien Hivemind Fix (#6405)
## About The Pull Request
hiveminds were broken in #5502 due to aliens lacking lungs on message
mod checks. this PR adds a check for lungless mobs to rectify that.

## Why It's Good For The Game
it's nice to use aliens in events sometimes

## Changelog

:cl:
fix: Alien hiveminds should be functional again.
/:cl:

code/modules/mob/living/carbon/carbon_say.dm
@@ -16,7 +16,7 @@
 
 /mob/living/carbon/get_message_mods(message, list/mods)
 	var/obj/item/organ/lungs/our_lungs = getorganslot(ORGAN_SLOT_LUNGS)
-	if(our_lungs?.received_pressure_mult < 0.5 && !mods[WHISPER_MODE] && !HAS_TRAIT(src, TRAIT_NOBREATH))
+	if(our_lungs && our_lungs.received_pressure_mult < 0.5 && !mods[WHISPER_MODE] && !HAS_TRAIT(src, TRAIT_NOBREATH))
 		mods[WHISPER_MODE] = MODE_WHISPER
 	return ..(message, mods)
 